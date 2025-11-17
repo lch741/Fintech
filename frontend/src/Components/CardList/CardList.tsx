5f41b0e0b1a83f40b6f97969d33e859e509d19cf
Chenghong Li <lch741@foxmail.com>
2025-11-17 14:29:40 +1300

    Finished search and show campany list

---
+++ frontend/src/Components/CardList/CardList.tsx
@@ -0,0 +1,26 @@
+import Card from '../Card/Card'
+import React from 'react'
+import { CompanySearch } from "../../company";
+import { v4 as uuidv4 } from "uuid";
+
+interface Props {
+  searchResults: CompanySearch[];
+}
+
+const CardList: React.FC<Props> = ({ searchResults }: Props): React.JSX.Element => {
+  return (
+    <>
+      {searchResults.length > 0 ? (
+        searchResults.map((result) => {
+          return (
+            <Card id={result.symbol} key={uuidv4()} searchResult={result} />
+          );
+        })
+      ) : (
+        <h1>No results!</h1>
+      )}
+    </>
+  );
+};
+
+export default CardList;
