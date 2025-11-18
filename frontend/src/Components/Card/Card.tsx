158fa52add353c20977454750c931fc2c4715b4b
Chenghong Li <lch741@foxmail.com>
2025-11-18 14:00:22 +1300

    Use tailwindcss to improve the appearance

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import "./Card.css";
 import { CompanySearch } from "../../company";
 import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
 
-interface Props{
+interface Props {
   id: string;
   searchResult: CompanySearch;
-  onPortfoliocreate: (e: React.SyntheticEvent) => void;
+  onPortfoliocreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({id, searchResult, onPortfoliocreate}: Props) : React.JSX.Element => {
+const Card: React.FC<Props> = ({
+  id,
+  searchResult,
+  onPortfoliocreate,
+}: Props): React.JSX.Element => {
   return (
-    <div key={id} id={id} className="card">
-      
-        <div className="details">
-          <h2>
-            {searchResult.name} ({searchResult.symbol})
-          </h2>
-          <p>{searchResult.currency}</p>
-        </div>
-
-        <div>
-            <p className="infon">
-                {searchResult.exchange} - {searchResult.exchangeFullName}
-            </p>
-            <AddPortfolio onPortfoliocreate={onPortfoliocreate} Symbol={searchResult.symbol} />
-        </div>
-
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
+        {searchResult.exchange} - {searchResult.exchangeFullName}
+      </p>
+      <AddPortfolio
+        onPortfoliocreate={onPortfoliocreate}
+        Symbol={searchResult.symbol}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
