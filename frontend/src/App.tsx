a932ed8fddede7f12c0f7275ebc0e98abb332fc3
Chenghong Li <lch741@foxmail.com>
2025-11-18 15:57:37 +1300

    Add router and different pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,59 +1,13 @@
-import { ChangeEvent, SyntheticEvent, useState } from "react";
-import "./App.css";
-import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Search/Search";
-import { searchCompanies } from "./api";
-import { CompanySearch } from "./company";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
-import Hero from "./Components/Hero/Hero";
+import { Outlet } from "react-router";
 import Navbar from "./Components/Navbar/Navbar";
-
+import "./App.css";
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string | null>(null);
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-
-  const onPortfoliocreate = (e: any) => {
-    e.preventDefault();
-    const exists = portfolioValues.find((value) => value === e.target[0].value);
-    if (exists) return;
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-  };
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value)=>{
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  };
-
-  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-  const onSearchSubmit = async(e: SyntheticEvent) => {
-    e.preventDefault();
-    const result = await searchCompanies(search);
-    if (typeof result === "string") {
-      setServerError(result);
-      console.log(serverError);
-    }else if(Array.isArray(result.data)){
-      setSearchResult(result.data);
-    }
-    console.log(searchResult);
-  };
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
-      <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete = {onPortfolioDelete} />
-      <CardList searchResults={searchResult} onPortfoliocreate={onPortfoliocreate} />
-      {serverError && <div>Unable to connect to API</div>}
-    </div>
+      <Outlet />
+    </>
   );
 }
 
