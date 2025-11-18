c59c8e1d97ef50cc6e0018e80416ad11e95660f3
Chenghong Li <lch741@foxmail.com>
2025-11-18 22:20:31 +1300

    Finished getCompanyProfile api, tested CompanyPage

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from '../../api';
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+  let { ticker } = useParams<{ ticker: string }>();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(()=>{
+    const getProfileInit = async () =>{
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  },[]);
   return (
-    <div>CompanyPage</div>
-  )
-}
+    <>
+      {company ? (
+        <div className="company-profile-container">{company.companyName}</div>
+      ) : (
+        <div>Company Not Found!</div>
+      )}
+    </>
+  );
+};
 
 export default CompanyPage
