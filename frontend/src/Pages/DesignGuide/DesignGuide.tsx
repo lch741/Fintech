9dc655d8adfad22ad48ef88bba0abd20a690ad9c
Chenghong Li <lch741@foxmail.com>
2025-11-20 13:45:29 +1300

    Add CompanyKeyMetrics api and CompanyProfile Page

--- frontend/src/Pages/DesignGuide/DesignGuide.tsx
+++ frontend/src/Pages/DesignGuide/DesignGuide.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Table from "../../Components/Table/Table";
 import RatioList from "../../Components/RatioList/RatioList";
+import { TestDataCompany } from "../../Components/Table/testData";
+import { CompanyKeyMetrics } from "../../company";
 
 type Props = {};
+
+const data = TestDataCompany;
+
+const tableConfig = [
+  {
+    label: "symbol",
+    render: (company: any) => company.marketCapTTM,
+    subtitle:"Total value of all a comapny's shares of stock"
+  },
+];
 
 const DesignGuide = (props: Props) => {
   return (
@@ -11,7 +23,7 @@ const DesignGuide = (props: Props) => {
         Design guide- This is the design guide for Fin Tech. These are reuable
         components of the app with brief instructions on how to use them.
       </h1>
-      <RatioList />
+      <RatioList config={tableConfig} data={data} />
       <Table />
       <h3>
         Table - Table takes in a configuration object and company data as
