78b0afd0bab28735b32dcfbaac7a9257bb9d65cb
Chenghong Li <lch741@foxmail.com>
2025-11-22 22:18:52 +1300

    Change Portfolio card link to company-profile

--- frontend/src/Components/RatioList/RatioList.tsx
+++ frontend/src/Components/RatioList/RatioList.tsx
@@ -30,7 +30,7 @@ const RatioList = ({ config, data }: Props) => {
     ); 
   });
   return (
-    <div className="bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6w-full max-w-full overflow-x-auto break-words">
+    <div className="bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6w-full max-w-screen-xl overflow-x-auto break-words">
       <ul className="divide-y divide-gray-200">{renderedCells}</ul>
     </div>
   );
