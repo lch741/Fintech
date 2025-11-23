e80196669560e280aced053b5d16539a34d3e258
Chenghong Li <lch741@foxmail.com>
2025-11-23 21:03:29 +1300

    Formated numbers and improved sheets

--- frontend/src/Components/CompanyProfile/CompanyProfile.tsx
+++ frontend/src/Components/CompanyProfile/CompanyProfile.tsx
@@ -4,25 +4,63 @@ import { getKeyMetrics } from "../../api";
 import RatioList from "../RatioList/RatioList";
 import { useOutlet, useOutletContext } from 'react-router';
 import Spinner from '../Spinner/Spinner';
+import {formatLargeNonMonetaryNumber,formatRatio,} from "../../Helpers/NumberFormatting";
 
 interface Props {}
 
 const tableConfig = [
   {
     label: "Market Cap",
-    render: (company: CompanyKeyMetrics) => company.marketCap,
+    render: (company: CompanyKeyMetrics) =>
+      formatLargeNonMonetaryNumber(company.marketCap),
+    subTitle: "Total value of all a company's shares of stock",
   },
   {
     label: "Current Ratio",
-    render: (company: CompanyKeyMetrics) => company.currentRatioTTM,
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.currentRatioTTM),
+    subTitle:
+      "Measures the companies ability to pay short term debt obligations",
   },
   {
     label: "Return On Equity",
-    render: (company: CompanyKeyMetrics) => company.returnOnEquityTTM,
+    render: (company: CompanyKeyMetrics) => formatRatio(company.returnOnEquityTTM),
+    subTitle:
+      "Return on equity is the measure of a company's net income divided by its shareholder's equity",
   },
   {
-    label: "Free Cash Flow To Equity",
-    render: (company: CompanyKeyMetrics) => company.freeCashFlowToEquityTTM,
+    label: "Return On Assets",
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.returnOnTangibleAssetsTTM),
+    subTitle:
+      "Return on assets is the measure of how effective a company is using its assets",
+  },
+  {
+    label: "Free Cash Flow Yield TTM",
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.freeCashFlowYieldTTM),
+    subTitle:
+      "Return on the financial performance of a company by comparing its free cash flow per share to its market price per share",
+  },
+  {
+    label: "Earnings Yield TTM",
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.earningsYieldTTM),
+    subTitle: "Showshow much a company earns relative to its Enterprise Value",
+  },
+  {
+    label: "Capex to revenue TTM",
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.capexToRevenueTTM),
+    subTitle:
+      "Capex to revenue provides insight into a company's investment strategy, capital intensity, and efficiency",
+  },
+  {
+    label: "Graham Number",
+    render: (company: CompanyKeyMetrics) =>
+      formatRatio(company.grahamNumberTTM),
+    subTitle:
+      "This is the upperbouind of the price range that a defensive investor should pay for a stock",
   },
 ];
 
