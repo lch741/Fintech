cc5957b100fec7790dc517366e4bd0fa80eec84e
Chenghong Li <lch741@foxmail.com>
2025-11-25 11:43:38 +1300

    Add repository layer and its dependency injection

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Interfaces;
 using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -16,15 +17,17 @@ namespace api.Controllers
     public class StockController : ControllerBase
     {
         private readonly ApplicationDBContext _context;
-        public StockController(ApplicationDBContext context)
+        private readonly IStockRepository _stockRepo;
+        public StockController(ApplicationDBContext context,IStockRepository stockRepo)
         {
             _context = context;
+            _stockRepo = stockRepo;
         }
         
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var stocks = await  _context.Stock.ToListAsync();
+            var stocks = await  _stockRepo.GetAllAsync();
             var stockDto = stocks.Select(s => s.ToStockDto());
             return Ok(stockDto);
         }
