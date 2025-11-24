b9aab801cf116da4e71f56ed257e6dbaecd66f2b
Chenghong Li <lch741@foxmail.com>
2025-11-24 14:46:45 +1300

    Add select controllers and DTO

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5793604a8401288144a0cc4595d8d50165b16579")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b062c186a98d8426faa966ea0ce2becea32982f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
