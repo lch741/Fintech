de4ecda43bde30f622eac95292abf52e37cf6e2f
Chenghong Li <lch741@foxmail.com>
2025-11-25 14:46:11 +1300

    Add GetById function to Comment

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435fe77cc0ab525b02df4f3d4c2898dcec90d585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca90fe60fd98cfd7fbdc0dfd9f526eb110f8ed80")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
