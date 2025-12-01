3f5aae876029194f66336fe85310962c21a0842a
Chenghong Li <lch741@foxmail.com>
2025-12-01 14:26:42 +1300

    Add FMP service  for stock  and comment controllers

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -19,11 +19,13 @@ namespace api.Controllers
         private readonly ICommentRepository _commentRepo;
         private readonly IStockRepository _stockRepo;
         private readonly UserManager<AppUser> _userManager;
-        public CommentController(ICommentRepository commentRepository,IStockRepository stockRepo,UserManager<AppUser> userManager)
+        private readonly IFMPService _fmpService;
+        public CommentController(ICommentRepository commentRepository,IStockRepository stockRepo,UserManager<AppUser> userManager,IFMPService fmpService)
         {
             _commentRepo = commentRepository;
             _stockRepo = stockRepo;
             _userManager = userManager;
+            _fmpService = fmpService;
         }
 
         [HttpGet]
@@ -39,6 +41,9 @@ namespace api.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comment = await _commentRepo.GetByIdAsync(id);
 
             if(comment == null)
@@ -49,17 +54,30 @@ namespace api.Controllers
             return Ok(comment.ToCommentDto());
         }
 
-        [HttpPost("{StockId}")]
-        public async Task<IActionResult> Create([FromRoute] int StockId, CreateCommentDto commentDto)
+        [HttpPost]
+        [Route("{symbol:alpha}")]
+        public async Task<IActionResult> Create([FromRoute] string symbol, CreateCommentDto commentDto)
         {
-            if(!await _stockRepo.StockExists(StockId))
-            {
-                return BadRequest("Stock does not exist");
-            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
+            var stock = await _stockRepo.GetBySymbolAsync(symbol);
+
+            if (stock == null)
+            {
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+                if(stock == null)
+                {
+                    return BadRequest("Stock does not exists");
+                }
+                else
+                {
+                    await _stockRepo.CreateAsync(stock);
+                }
+            }
             var username = User.GetUsername();
             var appUser = await _userManager.FindByNameAsync(username);
-            var commentModel = commentDto.ToCommentFromCreate(StockId);
+            var commentModel = commentDto.ToCommentFromCreate(stock.Id);
             commentModel.AppUserId = appUser!.Id;
             await _commentRepo.CreateAsync(commentModel);
             return CreatedAtAction(nameof(GetById),new{id=commentModel.Id},commentModel.ToCommentDto());
@@ -69,6 +87,9 @@ namespace api.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+                
             var comment = await _commentRepo.DeleteAsync(id);
 
             if(comment==null)
@@ -83,6 +104,9 @@ namespace api.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comment = await _commentRepo.UpdateAsync(id,updateDto.ToCommentFromUpdate());
             if (comment == null)
             {
