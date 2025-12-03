96a0a1c2f5a8ce4c8ea27d06493460242c3992c2
Chenghong Li <lch741@foxmail.com>
2025-12-03 14:10:10 +1300

    Add Register page

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -73,7 +73,7 @@ namespace api.Controllers
             if(!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var user = await _userManager.Users.FirstOrDefaultAsync(x=>x.UserName == loginDto.Username.ToLower());
+            var user = await _userManager.Users.FirstOrDefaultAsync(x=>x.UserName == loginDto.Username);
 
             if(user == null)
                 return Unauthorized("Invalid username!");
