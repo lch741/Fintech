8b8b2629dfdeb23089943de39daf03c1d1fd41d1
Chenghong Li <lch741@foxmail.com>
2026-03-27 00:26:50 +1300

    Enable Swagger UI in all environments

--- api/Program.cs
+++ api/Program.cs
@@ -100,11 +100,8 @@ builder.Services.AddHttpClient<IFMPService,FMPService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 app.UseAuthentication();
