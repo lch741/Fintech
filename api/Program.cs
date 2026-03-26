efa52dff8987cde79fa99fa42c817ef4c7fa8cd3
Chenghong Li <lch741@foxmail.com>
2026-03-26 23:40:40 +1300

    Set application to listen on http://0.0.0.0:80

--- api/Program.cs
+++ api/Program.cs
@@ -12,6 +12,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
