e0726ac50fe7d37bb39be15bd0d9b360a6f2f201
Chenghong Li <lch741@foxmail.com>
2026-03-29 21:49:57 +1300

    Change database provider from SQLite to PostgreSQL in DbContext configuration

--- api/Program.cs
+++ api/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
