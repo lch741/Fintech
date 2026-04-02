be134cbe4cd973adcde636df8d6bac19a6b19e0a
Chenghong Li <lch741@foxmail.com>
2026-04-03 00:05:31 +1300

    Update page to sign-up prompt and link

--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -115,15 +115,6 @@ const RegisterPage = (props: Props) => {
               >
                 Sign in
               </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Sign up
-                </a>
-              </p>
             </form>
           </div>
         </div>
